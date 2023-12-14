c24ec257c4b774ae1f7cbf1630bf27e5e828e82f
Christoph Gödecke <cgodecke@student.42wolfsburg.de>
2023-12-14 15:58:47 +0100

    added ex00

---
+++ ex00/easyfind.hpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cgodecke <cgodecke@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/14 14:30:56 by cgodecke          #+#    #+#             */
+/*   Updated: 2023/12/14 15:52:41 by cgodecke         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+# include<algorithm>
+# include<exception>
+class NotFoundExeption : public std::exception
+{
+	public:
+		virtual const char* what() const throw()
+		{
+			return ("Number not found");
+		}
+	
+};
+
+template <typename T>
+typename T::iterator easyfind(T &container, int n)
+{
+	typename T::iterator it;
+
+	it = std::find(container.begin(), container.end(), n);
+	if (it == container.end())
+		throw NotFoundExeption();
+	return (it);
+}
+
+#endif
