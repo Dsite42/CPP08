265b2585a4cc4f230ee60ecb754b9e0d564f2e0d
Christoph Gödecke <cgodecke@student.42wolfsburg.de>
2023-12-15 19:35:15 +0100

    added ex01

---
+++ ex01/Span.hpp
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Span.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cgodecke <cgodecke@student.42wolfsburg.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/15 18:38:29 by cgodecke          #+#    #+#             */
+/*   Updated: 2023/12/15 18:58:32 by cgodecke         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SPAN_HPP
+# define SPAN_HPP
+
+# include <iostream>
+# include <vector>
+# include <algorithm>
+# include <exception>
+
+class Span
+{
+private:
+	unsigned int		_N;
+	std::vector<int>	_v;
+
+public:
+	// Constructors
+	Span();
+	Span(unsigned int N);
+	Span(const Span &other);
+	Span &operator=(const Span &other);
+	~Span();
+
+	// Member functions
+	void	addNumber(int n);
+	int		shortestSpan();
+	int		longestSpan();
+	void 	addNumber(std::vector<int>::iterator begin, std::vector<int>::iterator end);
+	
+
+	// Exceptions
+	class FullContainerException : public std::exception
+	{
+		public:
+			virtual const char *what() const throw();
+	};
+
+	class NotEnoughNumbersException : public std::exception
+	{
+		public:
+			virtual const char *what() const throw();
+	};
+};
+
+#endif
+	
